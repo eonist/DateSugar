5116949a99a7ca1f6afeba2077f9501435f6aa56
Eonist <30n1st@gmail.com>
2018-12-08 14:59:15 +0100

    Init

---
+++ DateSugar/DateSugar.h
@@ -0,0 +1,19 @@
+//
+//  DateSugar.h
+//  DateSugar
+//
+//  Created by Eon on 08/12/2018.
+//  Copyright © 2018 FutureLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DateSugar.
+FOUNDATION_EXPORT double DateSugarVersionNumber;
+
+//! Project version string for DateSugar.
+FOUNDATION_EXPORT const unsigned char DateSugarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateSugar/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DateSugar
-Date sugar
+
+### What is it:
+Date 🔸 sugar
+
+### How does it work:
+`let date = Date(); sleep(3); date.secsSinceStart//3.0`
+
+### How do i get it:
+- Download or fork it
