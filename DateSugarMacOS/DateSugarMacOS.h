868394033c5da4f6f2c9a306bc7cec7a5f2eb500
Eonist <30n1st@gmail.com>
2019-07-11 11:55:43 +0200

    Add macOS framework target

---
+++ DateSugarMacOS/DateSugarMacOS.h
@@ -0,0 +1,19 @@
+//
+//  DateSugarMacOS.h
+//  DateSugarMacOS
+//
+//  Created by Eon on 11/07/2019.
+//  Copyright © 2019 FutureLab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DateSugarMacOS.
+FOUNDATION_EXPORT double DateSugarMacOSVersionNumber;
+
+//! Project version string for DateSugarMacOS.
+FOUNDATION_EXPORT const unsigned char DateSugarMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateSugarMacOS/PublicHeader.h>
+
+
